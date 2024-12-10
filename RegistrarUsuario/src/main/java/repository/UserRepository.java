3b5239bd5057a5a08fb2ebcdef48e11bd97b0082
Gael Franco Garcia <167834489+GaelFG17@users.noreply.github.com>
2024-12-09 23:08:33 -0600

    Delete RegistrarUsuario directory

--- RegistrarUsuario/src/main/java/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import model.User;
-//import com.example.userapi.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    // Métodos adicionales si necesitas búsquedas personalizadas
-}
